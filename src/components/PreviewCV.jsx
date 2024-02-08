cf8462583e7ec87c71a1062d808cce6ae2f6f4fe
Kenny Nguyen <sokenns@gmail.com>
2024-02-08 15:19:24 -0800

    Add placeholders for CV output

---
+++ src/components/PreviewCV.jsx
@@ -0,0 +1,26 @@
+function PreviewCV() {
+  return (
+    <div>
+      <div>
+        <h1>Full Name</h1>
+        <p>Email</p>
+        <p>Phone Number</p>
+      </div>
+      <div>
+        <h1>Education</h1>
+        <p>School Name</p>
+        <p>Degree</p>
+        <p>YYYY to YYYY</p>
+      </div>
+      <div>
+        <h1>Experience</h1>
+        <p>Company Name</p>
+        <p>Position</p>
+        <p>Description</p>
+        <p>YYYY to YYYY</p>
+      </div>
+    </div>
+  );
+}
+
+export default PreviewCV;
