7b19ce669c429590b57ac393abd82b1865c7b362
Kenny Nguyen <sokenns@gmail.com>
2024-02-08 15:20:01 -0800

    Display CV next to forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import Section from "./components/Section";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import PreviewCV from "./components/PreviewCV";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      <Section title="Contact">
-        <Contact />
-      </Section>
-      <Section title="Education">
-        <Education />
-      </Section>
-      <Section title="Experience">
-        <Experience />
-      </Section>
+      <div>
+        <Section title="Contact">
+          <Contact />
+        </Section>
+        <Section title="Education">
+          <Education />
+        </Section>
+        <Section title="Experience">
+          <Experience />
+        </Section>
+      </div>
+      <PreviewCV></PreviewCV>
     </>
   );
 }
