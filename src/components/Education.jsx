8266cdecbdeb1f75d0c7484606d1c75c0ce6efa8
Kenny Nguyen <sokenns@gmail.com>
2024-02-07 16:23:12 -0800

    Add form elements to education and experience components

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,27 @@
 function Education() {
-  return <form action=""></form>;
+  return (
+    <form action="">
+      <div className="form-field">
+        <label htmlFor="schoolName">School Name:</label>
+        <input type="text" name="schoolName" id="schoolName" />
+      </div>
+      <div className="form-field">
+        <label htmlFor="degree">Degree:</label>
+        <input type="text" name="degree" id="degree" />
+      </div>
+      <div className="form-field">
+        <label htmlFor="startYear">From:</label>
+        <input type="month" name="startYear" id="startYear" />
+        <label htmlFor="endYear">To:</label>
+        <input type="month" name="endYear" id="endYear" />
+      </div>
+      <div className="form-field">
+        <input type="checkbox" name="currentSchool" id="currentSchool" />
+        <label htmlFor="currentSchool">I am currently attending this school.</label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Education;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,31 @@
 function Experience() {
-  return <form action=""></form>;
+  return (
+    <form action="">
+      <div className="form-field">
+        <label htmlFor="company">Company Name:</label>
+        <input type="text" name="company" id="company" />
+      </div>
+      <div className="form-field">
+        <label htmlFor="position">Position Title:</label>
+        <input type="text" name="position" id="position" />
+      </div>
+      <div className="form-field">
+        <label htmlFor="desc">Description</label>
+        <textarea name="desc" id="desc" cols="30" rows="10"></textarea>
+      </div>
+      <div className="form-field">
+        <label htmlFor="startYear">From:</label>
+        <input type="month" name="startYear" id="startYear" />
+        <label htmlFor="endYear">To:</label>
+        <input type="month" name="endYear" id="endYear" />
+      </div>
+      <div className="form-field">
+        <input type="checkbox" name="currentJob" id="currentJob" />
+        <label htmlFor="currentJob">I am currently working at this job.</label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Experience;
