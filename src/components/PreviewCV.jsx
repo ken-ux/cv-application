92974bf169d474e2ab18b1dd3e1aab2a3f43c43a
Kenny Nguyen <sokenns@gmail.com>
2024-02-13 01:02:14 -0800

    Style preview CV section of page

--- src/components/PreviewCV.jsx
+++ src/components/PreviewCV.jsx
@@ -1,44 +1,58 @@
 import PropTypes from "prop-types";
+import "../styles/PreviewCV.css";
 
 function PreviewCV({ contact, education, experience }) {
   const schools = education.map((school) => {
+    const formattedStartYear = school.startYear.replace("-", "/");
+    const formattedEndYear = school.endYear.replace("-", "/");
+
     return (
-      <div key={school.id}>
-        <p>{school.schoolName}</p>
-        <p>{school.degree}</p>
+      <div className="school" key={school.id}>
         <p>
-          {school.startYear} to {school.endYear}
+          {formattedStartYear} to {formattedEndYear}
         </p>
+        <div>
+          <p className="school-name">{school.schoolName}</p>
+          <p>{school.degree}</p>
+        </div>
       </div>
     );
   });
 
   const jobs = experience.map((job) => {
+    const formattedStartYear = job.startYear.replace("-", "/");
+    const formattedEndYear = job.endYear.replace("-", "/");
+
     return (
-      <div key={job.id}>
-        <p>{job.companyName}</p>
-        <p>{job.position}</p>
-        <p>{job.description}</p>
+      <div className="job" key={job.id}>
         <p>
-          {job.startYear} to {job.endYear}
+          {formattedStartYear} to {formattedEndYear}
         </p>
+        <div>
+          <p className="company-name">{job.companyName}</p>
+          <p className="position-name">{job.position}</p>
+          <p>{job.description}</p>
+        </div>
       </div>
     );
   });
 
   return (
-    <div>
-      <div>
+    <div className="cv-container">
+      <div className="contact-container">
         <h1>{contact.name}</h1>
-        <p>{contact.email}</p>
-        <p>{contact.phoneNumber}</p>
+        <p>
+          {contact.email} | {contact.phoneNumber}
+        </p>
       </div>
-      <div>
-        <h1>Education</h1>
+      <div className="education-container">
+        <h2>Education</h2>
+        <hr />
         {schools}
       </div>
-      <div>
-        <h1>Experience</h1>
+      <div className="experience-container">
+        <h2>Experience</h2>
+        <hr />
         {jobs}
       </div>
     </div>
